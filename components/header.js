cae493e9a41d6d2aeb69bf0e69c61afac2db623a
bradymat <bradymat@gmail.com>
2016-12-08 20:07:22 +1300

    menu hidden on login

--- components/header.js
+++ components/header.js
@@ -1,12 +1,19 @@
 import html from 'yo-yo'
 
-module.exports = ({ title }, dispatch) => html`
-  <div>
-    <h1>${title}</h1>
-    <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/'}) }} >Home</p>
-    <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/play'}) }} >Play</p>
-    <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/mymap'}) }} >My Map</p>
-    <hr>
-  </div>
+module.exports = (state , dispatch) => {
+  const { title, route } = state
+  const showMenu = route == '/' ? 'hidden' : 'menu'
+  return html`
+    <div>
+      <h1>${title}</h1>
+      <div class=${showMenu}>
+        <ul>
+          <li onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/'}) }} >Home</li>
+          <li onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/play'}) }} >Play</li>
+          <li onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/mymap'}) }} >My Map</li>
+        </ul>
+      </div>
+      <hr>
+    </div>
 `
- 
+}

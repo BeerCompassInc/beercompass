759a9de268b87cd49680046e17cddc6a4b3777e5
Steve Sharp <zimsteve@live.com>
2016-12-08 19:41:28 +1300

    reinsert nav links aka header

--- components/index.js
+++ components/index.js
@@ -5,6 +5,7 @@ import link from './buttons/link'
 module.exports = (state, dispatch) => html
   `
     <div>
+      ${header(state, dispatch)}
       <img src="../images/compass-icon.png">
       <h1>Beer Compass</h1>
       <a href="https://www.google.com" alt="google login oauth" title="google login"><img src="../images/g-icon.png"></a>

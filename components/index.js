ee857b8cf8de3a7553cf83e60378495a56dc149b
Steve Sharp <zimsteve@live.com>
2016-12-08 19:30:35 +1300

    add png to deploy and title tag to a tags

--- components/index.js
+++ components/index.js
@@ -7,7 +7,7 @@ module.exports = (state, dispatch) => html
     <div>
       <img src="../images/compass-icon.png">
       <h1>Beer Compass</h1>
-      <a href="https://www.google.com" alt="google login oauth"><img src="../images/g-icon.png"></a>
-      <a href="https://www.facebook.com" alt="facebook login oauth"><img src="../images/fb-icon.png"></a>
+      <a href="https://www.google.com" alt="google login oauth" title="google login"><img src="../images/g-icon.png"></a>
+      <a href="https://www.facebook.com" alt="facebook login oauth" title="facebook login"><img src="../images/fb-icon.png"></a>
     </div>
   `

c3e50921f17e22837eac20039051f9081bf91d0b
bradymat <bradymat@gmail.com>
2016-12-08 16:47:18 +1300

    change route name

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ module.exports = ({ title }, dispatch) => html`
   <div>
     <h1>${title}</h1>
     <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/'}) }} >Home</p>
-    <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/info'}) }} >Info</p>
+    <p class='menu' onclick=${() => { dispatch({type: 'CHANGE_ROUTE', payload: '/play'}) }} >Info</p>
     <hr>
   </div>
 `

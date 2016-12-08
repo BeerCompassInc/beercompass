90e6ea00e991f2c67c982a9068dcd3a3ab0266d5
Steve Sharp <zimsteve@live.com>
2016-12-09 10:07:38 +1300

    part adding in google maps

--- components/mymap.js
+++ components/mymap.js
@@ -2,11 +2,28 @@ import html from 'yo-yo'
 import header from './header'
 import button from './buttons/custom'
 import link from './buttons/link'
+import request from 'superagent'
 
-module.exports = (state, dispatch) => html`
+module.exports = (state, dispatch) => {
+
+  request
+    .get('https://maps.googleapis.com/maps/api/js?key=AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI&callback=initMap')
+    .end((err, res) => {
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        })
+      }
+    })
+
+  return html`
     <div>
       ${header(state, dispatch)}
       ${button('back', dispatch, 'CHANGE_ROUTE', '/play')}
       <h1>MAP</h1>
+      <div id="map"></div>
     </div>
   `
+}

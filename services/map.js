63612b025421eaf5944bfb21efebe2d3c13c07b0
bradymat <bradymat@gmail.com>
2016-12-09 19:57:20 +1300

    map hides on different routes

--- services/map.js
+++ services/map.js
@@ -1,16 +1,15 @@
-const GoogleMapsLoader = require('google-maps')
+import GoogleMapsLoader from 'google-maps'
 
-function getMap(el){
-  console.log('element', el);
+GoogleMapsLoader.KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
+GoogleMapsLoader.REGION = 'GB'
 
-  GoogleMapsLoader.KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
+module.exports = (el) => {
   GoogleMapsLoader.load(function(google) {
+
       new google.maps.Map(el, {
-             zoom: 4,
-             center: {lat: 34, lng: -40.605}
+             zoom: 8,
+             center: {lat: 40.7128, lng: 74.0059}
            })
       console.log(google.maps)
   })
 }
-
-module.exports = getMap

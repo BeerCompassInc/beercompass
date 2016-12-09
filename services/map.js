fc67b732b27ac7f5d0873293f1eadfadac421e23
bradymat <bradymat@gmail.com>
2016-12-09 20:03:02 +1300

    services map refactor

--- services/map.js
+++ services/map.js
@@ -1,15 +1,16 @@
 import GoogleMapsLoader from 'google-maps'
 
 GoogleMapsLoader.KEY = 'AIzaSyDNqZpfY5wCQjq78QqttpZJ05714XxQTuI'
-GoogleMapsLoader.REGION = 'GB'
+GoogleMapsLoader.LIBRARIES = ['geometry', 'places']
 
 module.exports = (el) => {
-  GoogleMapsLoader.load(function(google) {
-
-      new google.maps.Map(el, {
-             zoom: 8,
-             center: {lat: 40.7128, lng: 74.0059}
-           })
-      console.log(google.maps)
+  GoogleMapsLoader.load((google) => {
+    new google.maps.Map(el, {
+       zoom: 4,
+       center: {
+         lat: 40.7128,
+         lng: 74.0059
+       }
+    })
   })
 }
